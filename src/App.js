529d574eec89d6b33a3672b9f499e15e9ae814b9
Yohanes Velly <yohanesvelly23@gmail.com>
2023-10-02 23:19:04 +0700

    workspace preparation sass, router dom, default path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import './assets/scss/style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LandingPageEN from './pages/en/LandingPage';
 import LandingPageID from './pages/id/LandingPage';
 
@@ -6,6 +10,7 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Navigate to="/en" />}></Route>
         <Route path="/en" element={<LandingPageEN />}></Route>
         <Route path="/id" element={<LandingPageID />}></Route>
       </Routes>
