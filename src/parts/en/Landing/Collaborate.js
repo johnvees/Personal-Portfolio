610f12093d0297ac8f4b888d7a7cfb64fd8baf36
Yohanes Velly <yohanesvelly23@gmail.com>
2023-10-12 02:26:44 +0700

    done styling but still unfixed

--- src/parts/en/Landing/Collaborate.js
+++ src/parts/en/Landing/Collaborate.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 export default function Collaborate() {
   return (
-    <div>
-      this is for collaborate landing
-    </div>
-  )
+    <section className="container-hero collaborate text-center">
+      <div
+        className=""
+        style={{
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div>
+          <h1>
+            Transform Your Ideas into Digital Magic Lets Collaborate for
+            Success!
+          </h1>
+          <div className="row align-items-center mt-4">
+            <div className="col-sm-2 col-md-2 col-lg-2 d-none d-md-block"></div>
+            <div className="col-sm-8 col-md-8 col-lg-8">
+              <button
+                type="button"
+                class="btn btn-primary "
+                style={{ width: '100%' }}
+              >
+                Get In Touch With Me!
+              </button>
+            </div>
+            <div className="col-sm-2 col-md-2 col-lg-2 d-none d-md-block"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
