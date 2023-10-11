aee3ae22fe405e77b7ee5e6c8c6e0c84fd5e44b7
Yohanes Velly <yohanesvelly23@gmail.com>
2023-10-12 01:45:19 +0700

    creating project content for landing, and still unfixed carousel

--- src/parts/en/Landing/Project.js
+++ src/parts/en/Landing/Project.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Project() {
+  
+
   return (
-    <div>
-      this if for project landing
-    </div>
-  )
+    <section className="container-hero project pt-5 pb-5">
+      <div className="row align-items-center mb-5">
+        <div className="col-sm-6 col-lg-6 flex-start">
+          <h3>PROJECTS</h3>
+          <h2>My Latest Projects Showcase</h2>
+        </div>
+        <div className=" d-flex ml-auto">
+          <div className="col">
+            <h5>Check out more projects I have worked on</h5>
+            <button
+              type="button"
+              class="btn btn-primary"
+              style={{ width: '100%' }}
+            >
+              Show More
+            </button>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+  );
 }
